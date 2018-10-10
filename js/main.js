11c17a7357b3c4deb1b5e41b6dc48138c8d21515
son.ho@edge-works.net <son.ho@edge-works.net>
2018-10-10 16:33:39 +0700

    project page scroll

--- js/main.js
+++ js/main.js
@@ -8,3 +8,41 @@ $(document).ready(function(){
         $(".header-menu-toggle").removeClass("hidden");
     });
 });
+
+(function($){
+    var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+    };
+
+     /* Smooth Scrolling
+    * ------------------------------------------------------ */
+   var clSmoothScroll = function() {
+        
+    $('.smoothscroll').on('click', function (e) {
+        var target = this.hash,
+        $target    = $(target);
+        
+            e.preventDefault();
+            e.stopPropagation();
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, cfg.scrollDuration, 'swing').promise().done(function () {
+
+            // check if menu is open
+            if ($('body').hasClass('menu-is-open')) {
+                $('.header-menu-toggle').trigger('click');
+            }
+
+            window.location.hash = target;
+        });
+    });
+
+};
+
+    (function ssInit() {
+        clSmoothScroll();
+    })();
+
+})(jQuery)
